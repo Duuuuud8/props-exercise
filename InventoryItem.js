cfd2ae6cd376a8c499768524f6a06f68452b413c
Duuuuud8 <jliles1997@yahoo.com>
2026-03-10 17:36:44 -0400

    Updated Complete Assignment

--- InventoryItem.js
+++ InventoryItem.js
@@ -6,7 +6,7 @@ function InventoryItem (props){
 				<p>Quantity: {props.quantity}</p>
 				<p>Price: ${props.price}</p>
 
-				{props.quantity < 5 && <Message>⚠️ Low Stock!</Message>}
+				{props.quantity > 0 && props.quantity < 5 && <Message>⚠️ Low Stock!</Message>}
 				{props.quantity*props.price > 1000 && <Message>Caution, high value item, may require extra security</Message>}
 				{props.quantity === 0 && <Message>🚨Alert! We ran out, order ASAP🚨</Message>}
 				{/* windows + . gives emoji */}
